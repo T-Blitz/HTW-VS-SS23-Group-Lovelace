fa204d8a1210e2ff5cafc2d517f0da971cd765f8
Selvanitha <103634230+Selvanitha@users.noreply.github.com>
2023-09-10 22:10:09 +0530

    Update Board.java

--- Board.java
+++ Board.java
@@ -2,6 +2,9 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 import javax.swing.border.Border;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.*;
 
 class imageC extends JLabel {
     public JLabel logo;
@@ -87,14 +90,16 @@ class RoundedBorder implements Border {
     }
 
     public void paintBorder(Component c, Graphics g, int x, int y, int width, int height) {
+
         g.setColor(bgc);
         // g.fillRoundRect(x, y, width - 1, height - 1, radius, radius);
         g.setColor(Color.BLACK);
         g.drawRoundRect(x, y, width - 1, height - 1, radius, radius);
+        // g.drawString("hello", x, y);
     }
 }
 
-class optionbtn extends JButton {
+class optionbtn extends JButton implements ActionListener {
     private String loc;
     private String id;
     private int locX;
@@ -103,8 +108,13 @@ class optionbtn extends JButton {
     private int Jheight;
     private Color btncolor;
     private Color txtcolor;
+    private Color pressedBackgroundColor;
+    private Color hoverBackgroundColor;
+    JButton logo;
+    Font mansalva;
 
-    public optionbtn(String Loc, int x, int y, int width, int height, String name, Color design, Color design1) {
+    public optionbtn(String Loc, int x, int y, int width, int height, String name, Color design, Color design1,
+            Color design2) {
         loc = Loc;
         locX = x;
         locY = y;
@@ -113,30 +123,53 @@ class optionbtn extends JButton {
         id = name;
         btncolor = design;
         txtcolor = design1;
+        pressedBackgroundColor = design2;
+        hoverBackgroundColor = design2;
+        try {
+            mansalva = Font.createFont(Font.TRUETYPE_FONT, new FileInputStream("fonts/Mansalva-Regular.ttf"));
+            GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+            ge.registerFont(Font.createFont(Font.TRUETYPE_FONT, new FileInputStream("fonts/Mansalva-Regular.ttf")));
+        } catch (IOException e) {
+            System.out.println(e);
+        } catch (FontFormatException e) {
+            System.out.println(e);
+        }
     }
 
     public JButton btnFun() {
-        JButton logo = new JButton(loc);
+
+        logo = new JButton(loc);
         // logo.addActionListener(this);
         logo.setToolTipText(id);
         logo.setFocusable(false);
         logo.setBackground(btncolor);
         logo.setForeground(txtcolor);
         logo.setBorder(new RoundedBorder(20, btncolor));
-        logo.setOpaque(false);
-        // logo.setContentAreaFilled(false);
+        logo.setOpaque(true);
+        logo.setContentAreaFilled(true);
         // logo.setBorderPainted(false);
         logo.setCursor(new Cursor(Cursor.HAND_CURSOR));
         logo.setBounds(locX, locY, Jwidth + 10, Jheight + 10);
+        logo.setFont(mansalva.deriveFont(25f));
+        logo.addActionListener(this);
         return logo;
     }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        System.out.println(e.getID());
+        // logo.setText("Roseindia.net");
+    }
+
 }
 
 class launchpage {
     public JFrame sc;
     buttonImage sett1 = new buttonImage();
     optionbtn mainbtn = new optionbtn("Offline", 90, 600, 210, 40, "Offline", new Color(253, 18, 18),
-            new Color(0, 0, 0));
+            new Color(0, 0, 0), new Color(244, 56, 56));
+    optionbtn online = new optionbtn("Online", 90, 680, 210, 40, "Offline", new Color(51, 253, 18),
+            new Color(0, 0, 0), new Color(244, 56, 56));
 
     public launchpage(JFrame scn, int width, int height) {
         scn.add(sett1.buttonImage("image/setting.png", width - 60, 5, 40, 40, "Setting"));
@@ -144,27 +177,68 @@ class launchpage {
         scn.add(sett1.buttonImage("image/history.png", 10, height - 55, 40, 40, "history"));
         scn.add(sett1.buttonImage("image/profile.png", width - 50, height - 55, 40, 40, "profile"));
         scn.add(mainbtn.btnFun());
+        scn.add(online.btnFun());
     }
 
 }
 
+class gameanimation {
+    private static Object obj = new Object();   
+    public gameanimation(JFrame scr) {
+        HashMap<Integer, Integer[]> prices = new HashMap<Integer, Integer[]>();
+        int[][] animation = { { 1, 9, 7, 4, 3, 2, 5 }, { 9, 1, 8, 7, 6, 4 }, { 1, 5, 7, 4, 6, 3, 8, 9, 2 } };
+
+        ArrayList<Object> arr = new ArrayList<Object>();
+        prices.put(1, new Integer[] { 65, 300 });
+        prices.put(2, new Integer[] { 145, 300 });
+        prices.put(3, new Integer[] { 225, 300 });
+        prices.put(4, new Integer[] { 65, 390 });
+        prices.put(5, new Integer[] { 145, 390 });
+        prices.put(6, new Integer[] { 225, 390 });
+        prices.put(7, new Integer[] { 65, 470 });
+        prices.put(8, new Integer[] { 145, 470 });
+        prices.put(9, new Integer[] { 225, 470 });
+        for (int j = 0; j < 1; j++) {
+            int count = 0;
+            for (int i = 0; i < animation[j].length; i++) {
+                 
+                JButton b = new JButton((count % 2 == 0) ? "X" : "O");
+                arr.add(b);
+                b.setFont(new Font("Dialog", Font.BOLD, 70));
+                b.setOpaque(false);
+                b.setForeground(new Color(149, 223, 255));
+                b.setContentAreaFilled(false);
+                b.setBorderPainted(false);
+                b.setFocusPainted(false);
+                b.setBackground(new Color(96, 96, 255));
+                if (count % 2 == 1) {
+                    b.setForeground(new Color(255, 255, 255));
+                }
+                b.setBounds(prices.get(animation[j][i])[0], prices.get(animation[j][i])[1], 100, 70);
+                scr.add(b);
+                count++;
+            }
+            //scr.getContentPane().remove(((JComponent) arr.get(0)));
+        }
+
+    }
+}
+
 public class Board {
     public static void main(String args[]) {
-
         int width = 390, height = 844;
         JFrame screen = new JFrame();
-        GameInterface Inf = new GameInterface(width, height);
-        // game play = new game(width,height);
+        launchpage l = new launchpage(screen, width, height);
         imageC icon = new imageC();
         screen.add(icon.imageC("image/logo.png", 10, 0, 60, 60));
-        launchpage l = new launchpage(screen, width, height);
-        // screen.add(play);
+        gameanimation l1 = new gameanimation(screen);
+        GameInterface Inf = new GameInterface(width, height);
         screen.add(Inf);
-
         screen.setSize(width, height);
         screen.setTitle("Tic tac toe");
         screen.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         screen.setVisible(true);
+        
     }
 
 }
