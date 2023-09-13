80741b1ac8112821cd3976ee6c7273d694594e14
Masik <ms_1995@web.de>
2023-09-13 12:52:34 +0200

    cancel Button

--- Login/loginController.java
+++ Login/loginController.java
@@ -1,4 +1,31 @@
 package Login;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.PasswordField;
+import javafx.stage.Stage;
+import javafx.event.ActionEvent;
+
 public class loginController {
+
+
+    @FXML
+    private Button cancelButton;
+
+
+
+    public void cancelButtonOnAction(ActionEvent event) {
+        Stage stage = (Stage) cancelButton.getScene().getWindow();
+        stage.close();
+    }
+
+
+
+
+
+
 }
