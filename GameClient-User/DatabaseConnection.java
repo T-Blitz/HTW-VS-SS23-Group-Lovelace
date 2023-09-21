a17d201d971990aaf9c96acbae000578738df5a2
Masik05 <ki.manuel.schroeder@htwsaar.de>
2023-09-21 20:16:35 +0200

    push DatabaseConnection

---
+++ GameClient-User/DatabaseConnection.java
@@ -0,0 +1,24 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.io.IOException;
+
+public class DatabaseConnection {
+    public Connection databaseLink;
+
+    public Connection getConnection() {
+        String databaseName = "lovelace";
+        String databaseUser = "";
+        String databasePassword = "";
+        String url = "jdbc:mysql//localhost/" +databaseName;
+
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            databaseLink = DriverManager.getConnection(url, databaseUser, databasePassword);
+        }catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        return databaseLink;
+    }
+}
