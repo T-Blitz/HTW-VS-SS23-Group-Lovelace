87cc6752871986b8aa93bd54fd3d6f6f7bc46f31
Masik05 <ki.manuel.schroeder@htwsaar.de>
2023-09-21 19:54:24 +0200

    Login Controller initial push

--- GameClient-User/LoginController.java
+++ GameClient-User/LoginController.java
@@ -1,2 +1,28 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import java.io.IOException;
+
 public class LoginController {
+    @FXML
+    private Button cancelButton;
+
+    @FXML
+    private TextField usernameTextfield;
+
+    @FXML
+    private TextField passwordTextfield;
+
+    @FXML
+    private Label loginMessageLabel;
+
 }
