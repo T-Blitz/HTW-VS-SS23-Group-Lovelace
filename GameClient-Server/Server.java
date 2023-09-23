576924b7aa941c3109f372fb770743cca8d4f0eb
kb-5 <104017821+kb-5@users.noreply.github.com>
2023-09-24 01:34:42 +0200

    Create Server

---
+++ GameClient-Server/Server.java
@@ -0,0 +1,17 @@
+//import RMI;
+//import DatabaseConnection;
+
+public class Server {
+    public static void main(String[] args) {
+        Server server = new Server();
+        server.start(args);
+    }
+
+
+    private void start(String[] args) {
+        //localhost
+        //Datenbank Verbindung
+        //Start Server RMI
+
+    }
+}
